1bebf6ce66fe353a9660f02ced8b79030752e472
javier <devjavicont@gmail.com>
2021-09-12 21:46:27 -0400

    iniciar repositorio

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+const Header = () => (
+  <header className="header">
+    <div className="img-container">
+      <img src="https://ux.ed.team/img/edteam-ux.svg" alt="icono de edcupcake"/>
+    </div>
+    <nav>
+      <ul>
+          <li>Inicio</li>
+          <li>Cupcake</li>
+      </ul>
+    </nav>
+  </header>
+)
+export default Header
