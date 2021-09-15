9c263a3c0196e22d1e7f28d70c2bf63385bc9949
javier <devjavicont@gmail.com>
2021-09-14 21:43:26 -0400

    el componente cupcakes ahora hace peticion a la API y los elementos que existan en la API se renderizan

--- src/components/pages/cupcakes.jsx
+++ src/components/pages/cupcakes.jsx
@@ -1,6 +1,30 @@
-const Cupcakes = () => (
-  <div>
+import { useEffect, useState } from "react"
+import Cupcake from "../cards/Cupcake"
+const Cupcakes = () => {
+
+  const [cupcakes, setCupcakes] = useState()
+
+
+  useEffect(() => {
+    fetch("http://localhost:3050/cupcake")
+      .then(response => response.json())
+      .then(data => setCupcakes(data))
+  }, [])
+
+  return (
+  <section>
     <h1>Pagina de Cupcakes</h1>
-  </div>
-)
+    {
+      cupcakes ? (
+        <div>
+          {
+            cupcakes.map(c => <Cupcake/>)
+          }
+        </div>
+      ) : (<span>cargando</span>)
+    }
+  </section>
+  )
+}
+
 export default Cupcakes
