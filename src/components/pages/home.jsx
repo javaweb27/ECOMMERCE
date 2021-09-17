7483eae23f549dd39bb1a6b719e15489fbdc8863
javier <devjavicont@gmail.com>
2021-09-17 16:28:29 -0400

    el banner ahora muestra cupcakes de chocolate

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,11 +1,16 @@
+import Cupcakes from "./cupcakes"
+
 const  Home = () => (
-  <section className="banner">
-    <img className="bg-img" alt="cupcakes EDcupcakes" src="https://images.unsplash.com/photo-1618652970214-19b55729f0e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1450&q=80"/>
-    <div className="container">
-      <h1>Bievenido a EDcupcake</h1>
-      <p>¡Un sitio lleno de sabor!</p>
-      <button>Ver cupcakes</button>
-    </div>
-  </section>
+  <>
+    <section className="banner">
+      <img className="bg-img" alt="cupcakes EDcupcakes" src="https://images.unsplash.com/photo-1618652970214-19b55729f0e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1450&q=80"/>
+      <div className="container">
+        <h1>Bievenido a EDcupcake</h1>
+        <p>¡Un sitio lleno de sabor!</p>
+        <button>Ver cupcakes</button>
+      </div>
+    </section>
+    <Cupcakes request="cupcakes?flavor_like=Chocolate"/>
+  </>
 )
 export default Home
