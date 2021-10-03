769a1a2af53c7dabf7e1412cc87c3567f5a49bca
javier <devjavicont@gmail.com>
2021-10-03 15:48:46 -0400

    el home ahora tienen el componente banner en lugar de puro HTML

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,16 +1,10 @@
-import Services from "../sections/Services"
 import Cupcakes from "./cupcakes"
+import Services from "../sections/Services"
+import Banner from "../sections/Banner"
 
 const  Home = () => (
   <>
-    <section className="banner">
-      <img className="bg-img" alt="cupcakes EDcupcakes" src="https://images.unsplash.com/photo-1618652970214-19b55729f0e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1450&q=80"/>
-      <div className="container">
-        <h1>Bievenido a EDcupcake</h1>
-        <p>¡Un sitio lleno de sabor!</p>
-        <button>Ver cupcakes</button>
-      </div>
-    </section>
+    <Banner/>
     <Cupcakes title="h2" request="cupcakes?flavor_like=Chocolate"/>
     <Services request="services"/>
   </>
