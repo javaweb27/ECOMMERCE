fd5f7d10b3b97de3f1c78b1d43cb58166cf6b9c3
javier <devjavicont@gmail.com>
2021-10-20 17:33:55 -0400

    se corrigio el alcance del CartProvider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,25 +12,25 @@ import CupcakesMostSoldProvider from "./context/CupcakesMostSoldProvider";
 import CartProvider from "./context/CartProvider";
 
 const App = () => (
-  <Router>
-    <CartProvider>
+  <CartProvider>
+    <Router>
       <Header/>
-    </CartProvider>
     
-    <Switch>
-      <Route path="/Proyecto-de-EDcupcake/cupcakes">
-        <CupcakesProvider request="cupcakes">
-          <Cupcakes title="h1"/>
-        </CupcakesProvider>
-      </Route>
+      <Switch>
+        <Route path="/Proyecto-de-EDcupcake/cupcakes">
+          <CupcakesProvider request="cupcakes">
+            <Cupcakes title="h1"/>
+          </CupcakesProvider>
+        </Route>
 
-      <Route path="/Proyecto-de-EDcupcake/">
-        <CupcakesMostSoldProvider request="cupcakes?flavor_like=Chocolate">
-          <Home/>
-        </CupcakesMostSoldProvider>
-      </Route>
-    </Switch>
-  </Router>
+        <Route path="/Proyecto-de-EDcupcake/">
+          <CupcakesMostSoldProvider request="cupcakes?flavor_like=Chocolate">
+            <Home/>
+          </CupcakesMostSoldProvider>
+        </Route>
+      </Switch>
+    </Router>
+  </CartProvider>
 )
 
 export default App;
