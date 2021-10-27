94d1127bae4b47b9b457a9fbc532e48bcb7c38c8
javier <devjavicont@gmail.com>
2021-10-26 21:04:40 -0400

    se creo los componentes sin contenido para las paginas de "iniciar sesion" y "registrarse"

---
+++ src/components/pages/login.jsx
@@ -0,0 +1,8 @@
+const Login = () => {
+
+  return (
+    <h1>Iniciar sesion</h1>
+  )
+}
+
+export default Login
---
+++ src/components/pages/register.jsx
@@ -0,0 +1,8 @@
+const Register = () => {
+
+  return (
+    <h1>Registrarse</h1>
+  )
+}
+
+export default Register
