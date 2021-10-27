695ea4c19155557d8fefe8cc3a2285ec2056cb73
javier <devjavicont@gmail.com>
2021-10-27 16:26:43 -0400

    se corrigio la indentacion

--- src/components/elements/RelativeLink.jsx
+++ src/components/elements/RelativeLink.jsx
@@ -2,12 +2,12 @@ import { NavLink } from "react-router-dom"
 import { BASE_PATH } from "../../BASE_PATH"
 
 const RelativeLink = ({path, text}) => (
-    <NavLink 
-      className="link" 
-      activeClassName="active-page" 
-      exact
-      to={BASE_PATH + path}
-    >{text}
-    </NavLink>
-)  
+  <NavLink 
+    className="link" 
+    activeClassName="active-page" 
+    exact
+    to={BASE_PATH + path}
+  >{text}
+  </NavLink>
+)
   export default RelativeLink
