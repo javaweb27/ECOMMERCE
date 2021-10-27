800ce2d44f622072dca832a10e693a0a71ba17ab
javier <devjavicont@gmail.com>
2021-10-27 19:48:07 -0400

    se agregaron las paginas publicas y la pagina 404

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,15 @@ import Home from "./pages/home";
 import AllCupcakes from "./pages/allCupcakes";
 import Login from "./pages/login";
 import Register from "./pages/register";
+import Page404 from "./pages/page404";
 import Protected from "./routes/Protected";
+import Public from "./routes/Public";
 import CartProvider from "./context/cart/CartProvider";
 import { BASE_PATH } from "../BASE_PATH";
 import {
   BrowserRouter as Router,
-  Switch
+  Switch,
+  Route
 } from "react-router-dom";
 
 const App = () => (
@@ -18,10 +21,10 @@ const App = () => (
       <Header/>
     
       <Switch>
-        <Protected 
+        <Public 
           path={BASE_PATH + "/login"}
           component={Login}/>
-        <Protected 
+        <Public 
           path={BASE_PATH + "/register"}
           component={Register}/>
         <Protected 
@@ -32,6 +35,11 @@ const App = () => (
           path={BASE_PATH + "/"}
           request="cupcakes?flavor_like=Chocolate"
           component={Home} />
+
+        <Route>
+          <Page404/>
+        </Route>
+        
       </Switch>
     </Router>
   </CartProvider>
