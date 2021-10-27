3935cd0a5cc60de9dfb2426cac88e33a02496b3a
javier <devjavicont@gmail.com>
2021-10-27 16:17:12 -0400

    el archivo APP_BASE_URL se renombro por BASE_PATH

--- src/components/elements/RelativeLink.jsx
+++ src/components/elements/RelativeLink.jsx
@@ -1,12 +1,13 @@
-import {NavLink} from "react-router-dom"
-import { APP_BASE_URL } from "../../APP_BASE_URL"
+import { NavLink } from "react-router-dom"
+import { BASE_PATH } from "../../BASE_PATH"
 
 const RelativeLink = ({path, text}) => (
     <NavLink 
       className="link" 
       activeClassName="active-page" 
       exact
-      to={`${APP_BASE_URL}${path}`}>{text}
+      to={BASE_PATH + path}
+    >{text}
     </NavLink>
 )  
   export default RelativeLink
