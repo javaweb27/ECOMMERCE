70b6ab0daae9e391b9dacfe4258faf8d6869a5db
javier <devjavicont@gmail.com>
2021-11-06 21:09:07 -0400

    se creo la componente de seccion "CartCupcakes"

---
+++ src/components/sections/CartCupcakes.jsx
@@ -0,0 +1,33 @@
+import { useContext } from "react"
+import Cupcake from "../cards/Cupcake"
+import CartContext from "../context/cart/CartContext"
+
+const CartCupcakes = () => {
+  const [state] = useContext(CartContext)
+  
+  return (
+    <section className="cart-cupcakes">
+      <h2>Cupcakes del carrito</h2>
+      <div className="container">
+        {
+          state.cart.map(({ id, description, img, flavor, color, price, sold }) => {
+            return (
+              <Cupcake
+                key={id}
+                id={id}
+                description={description}
+                img={img}
+                flavor={flavor}
+                color={color}
+                price={price}
+                sold={sold}
+              />
+            )
+          })
+        }
+      </div>
+    </section>
+  )
+}
+
+export default CartCupcakes
