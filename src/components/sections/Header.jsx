033571a1fc725483ab717b4d92ef9ab0582370cd
javier <devjavicont@gmail.com>
2021-12-01 16:54:29 -0400

    header: se cambio el color de los links y el nuevo icono tiene un ancho fijo

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -5,7 +5,7 @@ import CartCounter from "../elements/CartCounter"
 const Header = () => (
   <header className="header">
     <div className="img-container">
-      <img src="https://ux.ed.team/img/edteam-ux.svg" alt="icono de edcupcake"/>
+      <img src="https://images.unsplash.com/photo-1572451479139-6a308211d8be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="icono de edcupcake"/>
     </div>
     <span/>
     <nav>
