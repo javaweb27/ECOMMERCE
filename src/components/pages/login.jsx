33b445c61c4ee92099f7c90e09aff0137c25d1b5
javier <devjavicont@gmail.com>
2022-01-14 19:38:10 -0400

    Las paginas login y register ya no llaman a otro componente para su contenido

--- src/components/pages/login.jsx
+++ src/components/pages/login.jsx
@@ -1,9 +1,36 @@
-import LoginUser from "../sections/LoginUser"
+import { useState } from "react"
 
-const Login = () => (
+const LoginUser = () => {
+  const [data, setData] = useState({email: "", password: ""})
 
-  <LoginUser/>
-  
-)
+  const changeData = e => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value
+    })
+  }
 
-export default Login
+  const submit = e => {
+    e.preventDefault()
+    alert("iniciar sesion")
+  }
+
+  return (
+    <section>
+      <h1 className="title">Iniciar sesion</h1>
+      <form action="" onSubmit={submit} className="login">
+        <label htmlFor="email" className="data">Escribe tu correo
+          <input type="email" name="email" value={data.email} onChange={changeData} required/>
+        </label>
+
+        <label htmlFor="password" className="data">Escribe tu contraseña
+          <input type="password" name="password" value={data.password} onChange={changeData} required/>
+        </label>
+
+        <input type="submit" value="Ingresar" />
+      </form>
+    </section>
+  )
+}
+
+export default LoginUser
