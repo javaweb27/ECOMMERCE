24fa75aa7d2464ffb9aee8c29a71fecebd72a127
javier <devjavicont@gmail.com>
2022-01-15 17:57:31 -0400

    La pagina del carrito ya no importa otro componente para su contenido

--- src/components/pages/cart.jsx
+++ src/components/pages/cart.jsx
@@ -1,9 +1,37 @@
-import CartCupcakes from "../sections/CartCupcakes";
+import { useContext } from "react"
+import Cupcake from "../cards/Cupcake"
+import CartContext from "../context/cart/CartContext"
 
-const Cart = () => (
-
-  <CartCupcakes/>
-
-)
+const Cart = () => {
+  const [state] = useContext(CartContext)
+  
+  return (
+    <section className="cart-cupcakes">
+      <h2 className="title">
+        {state.cart.length ? 
+          "Cupcakes del carrito" : "No tienes cupcakes en el carrito"
+        }
+      </h2>
+      <div className="container">
+        {
+          state.cart.map(({ id, description, img, flavor, color, price, sold }) => {
+            return (
+              <Cupcake
+                key={id}
+                id={id}
+                description={description}
+                img={img}
+                flavor={flavor}
+                color={color}
+                price={price}
+                sold={sold}
+              />
+            )
+          })
+        }
+      </div>
+    </section>
+  )
+}
 
 export default Cart
