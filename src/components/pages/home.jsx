9c9f835eb624800ff6d1fcad795ee949d8025ecc
javier <devjavicont@gmail.com>
2022-01-16 15:09:08 -0400

    Home: el scroll se restaura al inicio en el renderizado

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -2,12 +2,18 @@ import Cupcakes from "../sections/Cupcakes"
 import Services from "../sections/Services"
 import Banner from "../sections/Banner"
 import ContentContainer from "../elements/ContentContainer"
+import { useEffect } from "react"
 
-const  Home = () => (
-  <ContentContainer headerForHome>
-    <Banner/>
-    <Cupcakes title="h2" specificCupcakes="MOST_SOLD"/>
-    <Services request="services"/>
-  </ContentContainer>
-)
+const  Home = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+  return (
+    <ContentContainer headerForHome>
+      <Banner/>
+      <Cupcakes title="h2" specificCupcakes="MOST_SOLD"/>
+      <Services request="services"/>
+    </ContentContainer>
+  )
+}
 export default Home
