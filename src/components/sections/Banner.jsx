56a4268780bd45d6cb9f9c3ac0c61eb4202a0a6b
javier <devjavicont@gmail.com>
2022-01-15 20:04:21 -0400

    Banner: cambia el fondo con el color de fondo

--- src/components/sections/Banner.jsx
+++ src/components/sections/Banner.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const Banner = () => (
   <section className="banner" id="banner-section">
-    <img className="bg-img" alt="cupcakes EDcupcakes" src="https://images.unsplash.com/photo-1618652970214-19b55729f0e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1450&q=80"/>
+    <img className="bg-img" alt="cupcakes EDcupcakes" src="https://images.unsplash.com/photo-1581978392302-136a082482e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80"/>
     <div className="container">
       <h1>Bievenido a EDcupcake</h1>
       <p>¡Un sitio lleno de sabor!</p>
