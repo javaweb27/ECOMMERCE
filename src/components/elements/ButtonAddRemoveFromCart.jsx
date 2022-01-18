26b4d4ac82ba4659037f88748919c38c0482697b
javier <devjavicont@gmail.com>
2022-01-18 19:54:21 -0400

    ButtonAddRemoveFromCart: sus estilos ahora son de tipo modulo css

--- src/components/elements/ButtonAddRemoveFromCart.jsx
+++ src/components/elements/ButtonAddRemoveFromCart.jsx
@@ -1,12 +1,12 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from "../context/actions"
 import Icon from "./icon"
+import styles from "../../styles/elements/button-add-remove-from-cart.module.scss"
 
 const ButtonAddRemoveFromCart = ({cartState, cartDispatch, cupcake}) => {
   const addToCart = () => cartDispatch({
     type: ADD_TO_CART,
     cupcake: cupcake
   })
-
   const removeFromCart = () => cartDispatch({
     type: REMOVE_FROM_CART,
     cupcake: cupcake
@@ -15,13 +15,13 @@ const ButtonAddRemoveFromCart = ({cartState, cartDispatch, cupcake}) => {
   return (
     <>
       { cartState.cart.find(c => c.id === cupcake.id) ? 
-        <button onClick={removeFromCart} className="text btn-remove">
-          <Icon name="cart_checked" className="bi-cart-checked"/>
-          <Icon name="cart_remove" className="bi-cart-remove"/>
+        <button onClick={removeFromCart} className={`text ${styles["btn-remove"]}`}>
+          <Icon name="cart_checked" className={styles["cart-checked"]}/>
+          <Icon name="cart_remove" className={styles["cart-remove"]}/>
         </button>
         :
-        <button onClick={addToCart} className="text btn-add">
-          <Icon name="cart" className="bi-cart"/>
+        <button onClick={addToCart} className={`text ${styles["btn-add"]}`}>
+          <Icon name="cart"/>
         </button>
       }
     </>
