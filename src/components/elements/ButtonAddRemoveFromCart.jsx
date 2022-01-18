ade2aebea1ee81bb3061cfbf41d77ecf1990abe9
javier <devjavicont@gmail.com>
2022-01-18 19:39:22 -0400

    icon svg: ahora se trata como componente y ButtonAddRemoveFromCart no usa la prop width

--- src/components/elements/ButtonAddRemoveFromCart.jsx
+++ src/components/elements/ButtonAddRemoveFromCart.jsx
@@ -2,7 +2,6 @@ import { ADD_TO_CART, REMOVE_FROM_CART } from "../context/actions"
 import Icon from "./icon"
 
 const ButtonAddRemoveFromCart = ({cartState, cartDispatch, cupcake}) => {
-
   const addToCart = () => cartDispatch({
     type: ADD_TO_CART,
     cupcake: cupcake
@@ -17,11 +16,13 @@ const ButtonAddRemoveFromCart = ({cartState, cartDispatch, cupcake}) => {
     <>
       { cartState.cart.find(c => c.id === cupcake.id) ? 
         <button onClick={removeFromCart} className="text btn-remove">
-          {Icon("cart_checked", "bi-cart-checked", "100%" )}
-          {Icon("cart_remove", "bi-cart-remove", "100%" )}
+          <Icon name="cart_checked" className="bi-cart-checked"/>
+          <Icon name="cart_remove" className="bi-cart-remove"/>
         </button>
         :
-        <button onClick={addToCart} className="text btn-add">{Icon("cart", "bi-cart", "100%" )}</button>
+        <button onClick={addToCart} className="text btn-add">
+          <Icon name="cart" className="bi-cart"/>
+        </button>
       }
     </>
   )
