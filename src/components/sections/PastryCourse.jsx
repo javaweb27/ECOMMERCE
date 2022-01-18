ecd003a32e9fd2e3f90c06997024a5718b24b7f2
javier <devjavicont@gmail.com>
2022-01-17 20:26:08 -0400

    Nueva seccion Curso de reposteria

---
+++ src/components/sections/PastryCourse.jsx
@@ -0,0 +1,13 @@
+const PastryCourse = () => {
+  return (
+    <section className="pastry-course">
+      <div className="container">
+        <h2 className="title">Curso de reposteria</h2>
+        <p className="description">Este es un curso desde cero que enseñara a hacer cupcakes</p>
+        <button className="button">Inscribirme</button>
+      </div>
+    </section>
+  )
+}
+
+export default PastryCourse
