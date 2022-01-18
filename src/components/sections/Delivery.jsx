a225f37b159fd6c60084507b902ff0a368414b1e
javier <devjavicont@gmail.com>
2022-01-17 20:26:39 -0400

    La seccion delivery ahora es independiente

---
+++ src/components/sections/Delivery.jsx
@@ -0,0 +1,10 @@
+const Delivery = () => {
+  return (
+    <section className="delivery">
+      <h2 className="title">Delivery a toda la cuidad</h2>
+      <p>Pide desde donde quieras y los cupcakes llegaran a tu casa</p>
+    </section>
+  )
+}
+
+export default Delivery
