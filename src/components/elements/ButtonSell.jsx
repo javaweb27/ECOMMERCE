a6938acb6bf6ac96c49c9eaa81fef8bcd434fdea
javier <devjavicont@gmail.com>
2022-01-18 20:06:45 -0400

    ButtonSell: sus estilos ahora son de tipo modulo css y usa el color primario

--- src/components/elements/ButtonSell.jsx
+++ src/components/elements/ButtonSell.jsx
@@ -1,8 +1,8 @@
 import useFetchPATCH from "../hooks/useFetchPATCH"
 import { SELL_CUPCAKE } from "../context/actions"
+import styles from "../../styles/elements/button-sell.module.scss"
 
 const ButtonSell= ({ id, sold, cupcakesState, cupcakesDispatch, cartDispatch}) => {
-
   const sell = () => {
     if (cupcakesState.cupcakes && !cupcakesState.error) {
       useFetchPATCH(id, cupcakesDispatch, cartDispatch)
@@ -28,9 +28,9 @@ const ButtonSell= ({ id, sold, cupcakesState, cupcakesDispatch, cartDispatch}) =
   return (
     <>
       { sold ? 
-        <span className="text sold-tag">vendido</span> 
+        <span className={`text ${styles["sold-tag"]}`}>vendido</span> 
         : 
-        <button onClick={sell} className="btn-sell">Vender</button>
+        <button onClick={sell} className={styles["btn-sell"]}>Vender</button>
       }
     </>
   )
