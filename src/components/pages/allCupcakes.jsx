734dc24b2bf3a598464cc58dd984b64306382501
javier <devjavicont@gmail.com>
2022-01-18 20:27:50 -0400

    Cupcakes: eliminado y su contenido ahora esta en allCupcakes con 1 titulo y ya no filtra cupcakes de chocolate

--- src/components/pages/allCupcakes.jsx
+++ src/components/pages/allCupcakes.jsx
@@ -1,10 +1,46 @@
+import { useContext } from "react"
+import CupcakesContext from "../context/cupcakes/CupcakesContext"
 import ContentContainer from "../elements/ContentContainer"
-import Cupcakes from "../sections/Cupcakes"
+import Cupcake from "../cards/Cupcake"
 
-const AllCupcakes = () => (
-  <ContentContainer>
-    <Cupcakes title="h1"/>
-  </ContentContainer>
-)
+const AllCupcakes = () => {
+  const {cupcakesState} = useContext(CupcakesContext)
+
+  return (
+    <ContentContainer>
+      <section className="cupcakes">
+        <h1 className="title">Todos los Cupcakes</h1>
+        <div className="container">
+          {
+            cupcakesState.cupcakes ? cupcakesState.cupcakes.map(({
+              id, 
+              description,
+              img,
+              flavor,
+              color,
+              price,
+              sold
+            }) => {
+              return (
+                <Cupcake
+                  key={id}
+                  id={id}
+                  description={description}
+                  img={img}
+                  flavor={flavor}
+                  color={color}
+                  price={price}
+                  sold={sold}
+                />
+              )
+            })
+            : 
+            <span>cargando</span>
+          }
+        </div>
+      </section>
+    </ContentContainer>
+  )
+}
 
 export default AllCupcakes
