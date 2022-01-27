65091002b3403ccfd244b07fb4610d2c4d51dfa3
javier <devjavicont@gmail.com>
2022-01-27 17:35:22 -0400

    CupcakesFeatures: importa Card Feature y cambian los estilos para responsive

--- src/components/sections/CupcakesFeatures.jsx
+++ src/components/sections/CupcakesFeatures.jsx
@@ -1,17 +1,12 @@
+import Feature from "../cards/Feature"
+
 const CupcakesFeatures = () => {
   return (
     <section className="cupcakes-features">
-      <div className="container">
-        <article className="feature">
-          <h3 className="title">Lorem ipsum dolor sit amet consectetur</h3>
-          <p className="description">Enim omnis, neque dignissimos perspiciatis ab quidem dolores at quos, Enim omnis, neque dignissimos perspiciatis ab quidem dolores at quos.</p>
-        </article>
-        <article className="feature">
-          <h3 className="title">Lorem ipsum dolor sit amet consectetur</h3>
-          <p className="description">Enim omnis, neque dignissimos perspiciatis ab quidem dolores at quos, Enim omnis, neque dignissimos perspiciatis ab quidem dolores at quos.</p>
-        </article>
-      </div>
-      <div className="img-container"><img src="https://images.unsplash.com/photo-1561817245-7af134029cea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80" alt="prepared cupcakes EDcupcake" loading="lazy" /></div>
+      <Feature/>
+      <Feature/>
+      <Feature/>
+      <Feature/>
     </section>
   )
 }
