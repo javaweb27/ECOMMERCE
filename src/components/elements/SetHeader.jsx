4eb1ea485fa17d38350f85af67ef39a49243659a
javier <devjavicont@gmail.com>
2022-04-22 16:00:22 -0400

    Header, Footer: ya no heredan color de texto

--- src/components/elements/SetHeader.jsx
+++ src/components/elements/SetHeader.jsx
@@ -1,25 +1,29 @@
+import { banner } from "../../styles/elements/menu-link.module.scss";
 import { useEffect } from "react"
 import getHeight from "../../functions/getHeight"
 
 const SetHeader = ({ headerForHome }) => {
+
   useEffect(() => {
     if (!headerForHome) return
 
     const header = document.querySelector(".header")
-    header.classList.add("fixed")
+
+    header.classList.add("fixed", banner)
     
     document.onscroll = () => {
       if (window.scrollY >= window.innerHeight - getHeight(header)) {
         header.classList.add("bg-active")
+        header.classList.remove(banner)
       }
       else {
         header.classList.remove("bg-active")
+        header.classList.add(banner)
       }
     }
     
     return () => {
-      header.classList.remove("fixed")
-      header.classList.remove("bg-active")
+      header.classList.remove("fixed", "bg-active", banner)
       document.onscroll = null
     }
   }, [])
