b7331260ac8c21926fa24d137afce8ea049c1f95
javier <devjavicont@gmail.com>
2022-04-23 16:42:15 -0400

    Agrega iconos al menu de navegacion al estar en footer

--- src/components/elements/MenuLink.jsx
+++ src/components/elements/MenuLink.jsx
@@ -1,12 +1,14 @@
 import { NavLink } from "react-router-dom"
 import styles from "../../styles/elements/menu-link.module.scss"
+import Icon from "./icon"
 
-const MenuLink = ({to, children}) => {
+const MenuLink = ({to, iconName, children}) => {
   const className = ({isActive}) => isActive ? `${styles.link} ${styles.active}` : styles.link
 
   return (
     <li className={styles.item}>
       <NavLink to={to} className={className}>
+        { iconName && <Icon name={iconName}/> }
         { children }
       </NavLink>
     </li>
--- src/components/elements/NavMenu.jsx
+++ src/components/elements/NavMenu.jsx
@@ -2,13 +2,17 @@ import styles from "../../styles/elements/nav-menu.module.scss"
 import MenuLink from "./MenuLink"
 import CartCounter from "./CartCounter"
 
-const NavMenu = ({header = false}) => {
+const NavMenu = ({ header = false }) => {
   return (
     <nav>
       <ul className={styles["nav-menu"]}>
-        <MenuLink to="/">Inicio</MenuLink>
-        <MenuLink to="/cupcakes">Cupcakes</MenuLink>
-        <MenuLink to="/cart">
+        <MenuLink to="/" iconName={header ? "" : "home" }>
+          Inicio
+        </MenuLink>
+        <MenuLink to="/cupcakes" iconName={header ? "" : "cupcake"}>
+          Cupcakes
+        </MenuLink>
+        <MenuLink to="/cart" iconName={header ? "" : "cart"}>
           <CartCounter/>
         </MenuLink>
         {
@@ -17,11 +21,13 @@ const NavMenu = ({header = false}) => {
             <MenuLink to="/register">Registrarse</MenuLink>
           </>
           : (
-            <MenuLink to="/profile">Perfil</MenuLink>
+            <MenuLink to="/profile" iconName={header ? "" : "profile"}>
+              Perfil
+            </MenuLink>
           )
         }
       </ul>
-  </nav>
+    </nav>
   )
 }
 
