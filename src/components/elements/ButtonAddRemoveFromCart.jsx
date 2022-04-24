82244ab5b705bbd36e9d2845bbc766851866bd3d
javier <devjavicont@gmail.com>
2022-04-24 18:44:56 -0400

    ButtonAddRemoveFromCard: corrige la altura y refactoriza

--- src/components/elements/ButtonAddRemoveFromCart.jsx
+++ src/components/elements/ButtonAddRemoveFromCart.jsx
@@ -4,25 +4,23 @@ import { useDispatch, useSelector } from "react-redux"
 import { addToCart, removeFromCart } from "../redux/reducers/cartSlice"
 
 const ButtonAddRemoveFromCart = ({ cupcake }) => {
-  const cart = useSelector(({cartSlice}) => cartSlice)
   const dispatch = useDispatch()
-  
+  const cart = useSelector(({ cartSlice }) => cartSlice)
+  const isInCart = cart.find(c => c.id === cupcake.id) ? true : false
+
   const add = () => dispatch(addToCart(cupcake))
   const remove = () => dispatch(removeFromCart(cupcake.id))
 
   return (
-    <>
-      { cart.find(c => c.id === cupcake.id) ? 
-        <button onClick={remove} className={`text ${styles["btn-remove"]}`}>
-          <Icon name="cart_checked" className={styles["cart-checked"]}/>
-          <Icon name="cart_remove" className={styles["cart-remove"]}/>
-        </button>
-        :
-        <button onClick={add} className={`text ${styles["btn-add"]}`}>
-          <Icon name="cart"/>
-        </button>
+    <button onClick={isInCart ? remove : add}
+      className={styles[isInCart ? "btn-remove" : "btn-add"]}
+    >
+      {isInCart ? <>
+        <Icon name="cart_checked" className={styles["cart-checked"]} />
+        <Icon name="cart_remove" className={styles["cart-remove"]} />
+      </> : <Icon name="cart" />
       }
-    </>
+    </button>
   )
 }
 
