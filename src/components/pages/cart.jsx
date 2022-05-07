a43d8c020fbf86c6a27c0f3db98e3594ff1a6d45
javier <devjavicont@gmail.com>
2022-05-07 18:37:01 -0400

    Cart: cambia ligeramente el diseño de los cupcakes

--- src/components/pages/cart.jsx
+++ src/components/pages/cart.jsx
@@ -5,34 +5,38 @@ import ContentContainer from "../elements/ContentContainer"
 
 const Cart = () => {
 
-  const cart = useSelector(({cartSlice}) => cartSlice)
+  const cart = useSelector(({ cartSlice }) => cartSlice)
 
   return (
     <ContentContainer>
       <section className="cart-cupcakes">
-        <h2 className="title">
-          {
-            cart.length ? "Cupcakes del carrito" : "No tienes cupcakes en el carrito"
-          }
-        </h2>
+        <h2 className="title">Carrito</h2>
+
         <div className="container">
-          {
-            cart.map(({ id, description, img, flavor, color, price, sold }) => {
-              return (
-                <Cupcake
-                  styles={styles}
-                  key={id}
-                  id={id}
-                  description={description}
-                  img={img}
-                  flavor={flavor}
-                  color={color}
-                  price={price}
-                  sold={sold}
-                />
-              )
-            })
-          }
+          <div>
+            <h3 className="title">
+              {cart.length ? "Cupcakes del carrito" : "No tienes cupcakes en el carrito"}
+            </h3>
+            <div className="cupcakes-container">
+              {
+                cart.map(({ id, description, img, flavor, color, price, sold }) => {
+                  return (
+                    <Cupcake
+                      styles={styles}
+                      key={id}
+                      id={id}
+                      description={description}
+                      img={img}
+                      flavor={flavor}
+                      color={color}
+                      price={price}
+                      sold={sold}
+                    />
+                  )
+                })
+              }
+            </div>
+          </div>
         </div>
       </section>
     </ContentContainer>
