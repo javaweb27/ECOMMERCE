7a9ca604ea91617c6e836289d6ce731efa360046
javier <devjavicont@gmail.com>
2022-05-17 19:37:49 -0400

    Evita volver a buscar al usuario si no hay saldo suficiente

--- src/routes/userRoutes/controllers/payUser.js
+++ src/routes/userRoutes/controllers/payUser.js
@@ -4,18 +4,22 @@ const UserModel = require("../userModel")
 
 function payUser(req, res) {
   const priceToPay = Number(req.body.amount)
-  const user = { email: req.dataFromToken.email }
+  const emailToFilter = { email: req.dataFromToken.email }
 
-  UserModel.findOne(user, (error, foundUser) => {
+  UserModel.findOne(emailToFilter, (error, foundUser) => {
     if (error) {
       console.error("Error al intentar buscar usuario para pagar", error);
       res.send(null)
       return
     }
+    const currentMoney = foundUser.money
+    if (currentMoney < priceToPay) {
+      console.log("Saldo insuficiente, no se pudo pagar")
+      res.json({ data: currentMoney })
+      return
+    }
 
-    const newMoney = foundUser.money >= priceToPay ? foundUser.money - priceToPay : foundUser.money
-
-    UserModel.findOneAndUpdate(user, { money: newMoney }, { new: true }, (error, updatedUser) => {
+    UserModel.findOneAndUpdate(emailToFilter, { money: currentMoney - priceToPay }, { new: true }, (error, updatedUser) => {
       res.json({ data: updatedUser.money })
     })
   })
