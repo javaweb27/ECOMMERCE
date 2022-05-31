f50570c6d1dbd1c257f43760d1e0efa5fef7a017
javier <devjavicont@gmail.com>
2022-05-31 18:55:25 -0400

    Input se vuelve .tsx

---
+++ src/components/elements/Input.tsx
@@ -0,0 +1,12 @@
+import classes from "../../styles/elements/input.module.scss"
+
+const Input = ({ i: inputName, name, value, children, ...params }: any) => {
+  return (
+    <label className={classes.label} htmlFor={name}>
+      <span>{children}</span>
+      <input id={name} className={classes.input} type={inputName} name={name} value={value} {...params} required />
+    </label>
+  )
+}
+
+export default Input
