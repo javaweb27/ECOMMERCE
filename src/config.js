e0453f40fc839ff2576d3cf7198382d77a588fce
javier <devjavicont@gmail.com>
2022-08-03 17:33:10 -0400

    Actualiza la url de CORS_ORIGIN_URL

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ import { config } from "dotenv"
 config()
 
 export const PORT = process.env.PORT || 3060
-export const CORS_ORIGIN_URL = process.env.CORS_ORIGIN_URL || "http://localhost:3000"
+export const CORS_ORIGIN_URL = process.env.CORS_ORIGIN_URL || "http://localhost:5173"
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/test"
 export const TOKEN_KEY = process.env.TOKEN_KEY
